2a21f76bd542d9b1b8641421ea806b789098f442
jakevenzkekondo <jake.vkondo@gmail.com>
2026-04-25 21:11:44 -0400

    End of section 9

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,14 +9,22 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider } from 'react-router'
 import { router } from './app/router/Routes.tsx'
+import { store, StoreContext } from './lib/stores/store.ts';
+import { unstable_batchedUpdates } from 'react-dom'
+import { configure } from 'mobx';
+
+configure({ reactionScheduler: unstable_batchedUpdates })
+
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <StoreContext.Provider value={store}>
+      <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools />
       <RouterProvider router={router} />
     </QueryClientProvider>
+    </StoreContext.Provider>
   </StrictMode>,
 )
