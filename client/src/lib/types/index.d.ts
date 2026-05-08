a2a66db3ca5711a68ce46f808ca366af5dc29fba
jakevenzkekondo <jake.vkondo@gmail.com>
2026-05-07 23:23:14 -0400

    End of section 15

--- client/src/lib/types/index.d.ts
+++ client/src/lib/types/index.d.ts
@@ -1,14 +1,26 @@
 type Activity = {
-  id: string
-  title: string
-  date: Date
-  description: string
-  category: string
-  isCancelled: boolean
-  city: string
-  venue: string
-  latitude: number
-  longitude: number
+    id: string
+    title: string
+    date: Date
+    description: string
+    category: string
+    isCancelled: boolean
+    city: string
+    venue: string
+    latitude: number
+    longitude: number
+    attendees: Profile[]
+    isGoing: boolean
+    isHost: boolean
+    hostId: string
+    hostDisplayName: string
+}
+
+type Profile = {
+    id: string
+    displayName: string
+    bio?: string
+    imageUrl?: string
 }
 
 type User = {
